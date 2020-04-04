e855fc27c21ae361921ddd164f5e72817217190b
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 18:38:33 -0500

    Create retirement.js

---
+++ retirement.js
@@ -0,0 +1,12 @@
+function retirement()
+  {
+  var total_savings = ((percentage / 100)* salary);
+  var employer_assist = (total_savings) * 0.35;
+                
+  var total_annaul_savings = total_savings + employer_assist;
+  var savings_duration = desired_savings / total_annaul_savings;
+  var age_obtained = savings_duration + age;
+  var age_obtained = age_obtained.toFixed(1);
+  }
+
+module.exports = retirement;
