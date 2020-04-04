ad92ce798915bbd44a8488e312d5febdff8dc7a7
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 18:39:07 -0500

    Update computeBMI.js

--- computeBMI.js
+++ computeBMI.js
@@ -1,9 +1,9 @@
 function computeBMI()
-{
-var weight = weight * 703;
-var height = height_feet * 12 + height_inches;
-var BMI = weight / (height * height);
-var BMI = BMI.toFixed(1);
-}
+  {
+    var weight = weight * 703;
+    var height = height_feet * 12 + height_inches;
+    var BMI = weight / (height * height);
+    var BMI = BMI.toFixed(1);
+  }
 
 module.exports = computeBMI;
