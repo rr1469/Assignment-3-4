ce4f78172b41d8ab0e10a363e5cc2c055a6912a9
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 19:11:49 -0500

    Update retirement.test.js

--- retirement.test.js
+++ retirement.test.js
@@ -2,5 +2,5 @@ retirement = require ('./retirement');
 test('Age:25, Salary:90000, 30% of Salary saved. Desired_Savings:800000, Meet your goal at: 46.9', () => {
     age_obtained = retirement(25, 90000, 30, 800000);
     years = age_obtained[0];
-    expect(years).toBe(69);
+    expect(years).toBe(46.9);
  });
