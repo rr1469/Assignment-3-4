acb6182d7ccef3ee58713f3e1b0314c653cf594c
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 20:47:33 -0500

    Update computeBMI.test.js

--- computeBMI.test.js
+++ computeBMI.test.js
@@ -8,6 +8,3 @@ test('BMI index of height 6 ft and 2 in, and 180 pounds equal 23.1', () => {
 test('BMI index of height 3 ft and 8 in, and 120 pounds equal 43.6', () => {
     expect (computeBMI(3,8,120)).toBe("43.6");
 });
-test('BMI index of height 5 ft and 11 in, and 250 pounds equal 34.9', () => {
-    expect (computeBMI(5,11,250)).toBe("34.9");
-});
