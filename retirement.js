3c4d451cce42ee159f301160b2512a7a8aa83afc
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 19:40:32 -0500

    Update retirement.js

--- retirement.js
+++ retirement.js
@@ -7,6 +7,7 @@ function retirement(age,salary,percentage,desired_savings)
   var savings_duration = desired_savings / total_annaul_savings;
   var age_obtained = savings_duration + age;
   var age_obtained = age_obtained.toFixed(1);
+  return age_obtained;  
   }
 
 module.exports = retirement;
