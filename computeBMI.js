c3e61043a06580b8b3c5843009cef4f24110ff96
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-04 19:23:20 -0500

    Update computeBMI.js

--- computeBMI.js
+++ computeBMI.js
@@ -1,5 +1,8 @@
-function computeBMI(height_feet,height_inches,weight)
+function computeBMI()
   {
+    var height_feet = Number(document.getElementById("height_feet").value);
+    var height_inches = Number(document.getElementById("height_inches").value);
+    var weight = Number(document.getElementById("weight").value);
     var weight = weight * 703;
     var height = height_feet * 12 + height_inches;
     var BMI = weight / (height * height);
