d929414e633a0a0355384cba4d60c0b0f4ba57f8
rr1469 <56932840+rr1469@users.noreply.github.com>
2020-04-07 23:41:33 -0500

    Update retirement.test.js

--- retirement.test.js
+++ retirement.test.js
@@ -4,3 +4,8 @@ test('Age:25, Salary:90000, 30% of Salary saved. Desired_Savings:800000, Meet yo
     years = age_obtained;
     expect(years).toBe("46.9");
  });
+test('Age:30, Salary:120000, 50% of Salary saved. Desired_Savings:3000000, Meet your goal at: 67.0', () => {
+    age_obtained = retirement(30, 120000, 50, 3000000);
+    years = age_obtained;
+    expect(years).toBe("67.0");
+ });
